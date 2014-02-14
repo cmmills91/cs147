09b66bcc8cb815ad3947a4235457b7cd07a8a47f
vagrant <vagrant@precise32.(none)>
2014-02-14 21:52:20 +0000

    package

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 var data = require('../data.json');
 var databaseUrl = "mongodb://admin:admin@ds033069.mongolab.com:33069/heroku_app22105721";
 
-//var databaseUrl = "mongodb://localhost:27017/mydb"
 var collections = ["tresfit", "arrgym"];
 var db = require("mongojs").connect(databaseUrl, collections);
 
@@ -11,6 +10,5 @@ var db = require("mongojs").connect(databaseUrl, collections);
 exports.view = function(req, res){
 	db.tresfit.find().toArray(function(err, items) {
         res.render('index', {'items': items});
-        console.log(items);
     });
 };
--- routes/station-started.js
+++ routes/station-started.js
@@ -1,7 +1,6 @@
 var data = require("../data.json");
 var databaseUrl = "mongodb://admin:admin@ds033069.mongolab.com:33069/heroku_app22105721";
 
-//var databaseUrl = "mongodb://localhost:27017/mydb"
 var collections = ["tresfit", "arrgym"];
 var db = require("mongojs").connect(databaseUrl, collections);
 
